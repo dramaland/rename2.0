8555a11619cc568fc4f703f6d187689756bb99f3
qwwerty12390 <139214618+qwwerty12390@users.noreply.github.com>
2024-01-26 11:26:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,4 +20,4 @@ START_PIC = os.environ.get("START_PIC", "")
 
 ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
-PORT = os.environ.get("PORT", "8080")
+PORT = os.environ.get("PORT", "1000")
