d20b41ce5613e5f01b15eac7e29412a08c3f7c96
qwwerty12390 <139214618+qwwerty12390@users.noreply.github.com>
2024-01-26 15:17:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "renamer",
+            name="renamer",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
@@ -24,30 +24,30 @@ class Bot(Client):
         )
 
     async def start(self):
-        await super().start()
-        me = await self.get_me()
-        self.mention = me.mention
-        self.username = me.username 
-        self.force_channel = FORCE_SUB
-        if FORCE_SUB:
-            try:
-                link = await self.export_chat_invite_link(FORCE_SUB)                  
-                self.invitelink = link
-            except Exception as e:
-                logging.warning(e)
-                logging.warning("Make Sure Bot admin in force sub channel")             
-                self.force_channel = None
-        app = web.Application()
-        app.add_routes([web.get('/', web_server)])
-        runner = web.AppRunner(app)
-        await runner.setup()
-        site = web.TCPSite(runner, '0.0.0.0', PORT)
-        await site.start()
-        logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
-        
+       await super().start()
+       me = await self.get_me()
+       self.mention = me.mention
+       self.username = me.username 
+       self.force_channel = FORCE_SUB
+       if FORCE_SUB:
+         try:
+            link = await self.export_chat_invite_link(FORCE_SUB)                  
+            self.invitelink = link
+         except Exception as e:
+            logging.warning(e)
+            logging.warning("Make Sure Bot admin in force sub channel")             
+            self.force_channel = None
+       app = web.AppRunner(await web_server())
+       await app.setup()
+       bind_address = "0.0.0.0"
+       await web.TCPSite(app, bind_address, PORT).start()
+       logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
+      
+
     async def stop(self, *args):
-        await super().stop()      
-        logging.info("Bot Stopped 🙄")
+      await super().stop()      
+      logging.info("Bot Stopped 🙄")
         
 bot = Bot()
 bot.run()
+
